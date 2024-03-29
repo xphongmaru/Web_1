7e851783b9c8a9a57214a6ed938168074f01560a
Phong <122958402+xphongmaru@users.noreply.github.com>
2024-03-29 16:17:56 +0700

    Add files via upload
    them bai gio hang

--- cart_delete.php
+++ cart_delete.php
@@ -6,9 +6,13 @@
         if($_SESSION['products'][$msp][3]!=1)
         {
             $_SESSION['products'][$msp][3]--;
+            $_SESSION['tt']-=$_SESSION['products'][$msp][2];
+            $_SESSION['sl']--;
         }
         else{
+            $_SESSION['tt']-=$_SESSION['products'][$msp][2];
             unset($_SESSION['products'][$msp]);
+            $_SESSION['sl']--;
         }
     }
     header('location:cart_detail.php')
--- cart_detail.php
+++ cart_detail.php
@@ -8,7 +8,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giỏi hàng</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
 <body>
@@ -26,28 +27,31 @@
                     <th>Action</th>
                 </thead>
                 <tbody>
-                    <!-- <tr>
-                        <td>SP01</td>
-                        <td>Ipnhone promax 15</td>
-                        <td>35.000.000</td>
-                        <td>5</td>
-                        <td><img src="https://cdn.tgdd.vn/Products/Images/42/305658/iphone-15-pro-max-blue-thumbnew-600x600.jpg" width="50px" alt=""></td>
-                        <td><a href="#"><button class="btn btn-primary">Thêm vào giỏ hàng</button></a></td>
-                    </tr> -->
                     <?php foreach($_SESSION['products'] as $key => $item) {?>
                         <tr>
                             <td><?php  echo $item[0] ?></td>
                             <td><?php  echo $item[1] ?></td>
-                            <td><?php  echo $item[2] ?></td>
+                            <td><?php  echo $item[2] ?><i class="bi bi-plus-square"></i></td>
                             <td><?php  echo $item[3] ?></td>
                             <td><img src="<?php  echo $item[4] ?>" alt="" width="50px"></td>
-                            <!-- <td><a href="addToCart.php?msp=<?php echo $item[0] ?>"><button class="btn btn-primary">Thêm vào giỏ hàng</button></a></td> -->
+            
                             <td><a href="cart_delete.php?msp=<?php echo $item[0]?>"><button class="btn btn-danger">Xóa sản phẩm</button></a></td>
                         </tr>
-                    <?php  }?>
+                    
+                   <?php  }?>
+                   <tr>
+                       <td>Tổng</td>
+                       <td colspan="2"></td>
+                       <td ><?php echo $_SESSION['sl'];?></td>
+                       
+                       <td colspan="2"> <?php echo "Thành tiền: ".$_SESSION['tt']." VNĐ";?></td>
+                   </tr>
                 </tbody>
+                
+
             </table>
-            <a href="cart_list.php" class="btn btn-success">Quay lại trang sản phẩm</a>
+            
+            <a href="cart_list.php" class="btn btn-success">Quay lại trang sản phẩm</a> 
         </div>
     </div>
 </body>
