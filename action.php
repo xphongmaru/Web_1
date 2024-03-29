d3ee434ed81a66b00ad106c12e58cd24250fd233
Phong <122958402+xphongmaru@users.noreply.github.com>
2024-03-29 16:18:56 +0700

    Add files via upload
    Thực hành ngày 29/3

---
+++ action.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Họ tên: ". $_GET['name']."<br>";
+    echo "Số điện thoại: ". $_GET['phone']."<br>";
+    echo "Email: ". $_GET['email']."<br>";
+    // echo "Giới tính: ". $_GET['gt']."<br>";
+    echo "ý kiến: ". $_GET['yk']."<br>";
+?>
